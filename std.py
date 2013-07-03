361b3791fc5a4fb92822c6de485e6dc13de46486
hswayne77 <hswayne77@gmail.com>
2013-07-03 15:48:40 +0800

    Update std.py

--- std.py
+++ std.py
@@ -2,9 +2,5 @@
 
 
 def std(list):
-    """
-        here we use 'standard deviation' to determine whether the system load is balanced,
-        where the sys_load value is small better.
-    """
     avg = sum(list)/float(len(list))
     return math.sqrt(sum(map(lambda x: (x-avg)**2,list))/len(list))
